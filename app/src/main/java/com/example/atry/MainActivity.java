a5f75b261c9fc2dfeee3268aac712aa5a718af29
Micheal <michealnkim-wm19@student.tarc.edu.my>
2020-09-11 18:31:24 +0800

    commit by ng

--- app/src/main/java/com/example/atry/MainActivity.java
+++ app/src/main/java/com/example/atry/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         drawer=findViewById(R.id.drawer_layout);
+
         ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
